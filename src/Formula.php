6408f04eb86f99b915d9a00d7f6079382b65cf33
jingdongli <1807996924@qq.com>
2025-04-30 14:38:09 +0800

    提交乘法口诀表

---
+++ src/Formula.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+class Formula
+{
+   public static function run()
+   {
+       for($i=1;$i<=9;$i++)
+       {
+           for($j=1;$j<=$i;$j++)
+           {
+               echo "{$j} * {$i} =". $j * $i."      ";
+           }
+           echo "".PHP_EOL;
+       }
+   }
+}
